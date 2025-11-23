7513ceed1e95c028a1c10c522842efc7cd60783a
Luci <Luci.Canet@gmail.com>
2025-11-23 17:29:07 -0500

    about

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -24,8 +24,8 @@ const AboutSectionOne = () => {
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="Crafted for Startup, SaaS and Business Sites."
-                paragraph="The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables."
+                title="Created for Small Business, by Small Business."
+                paragraph="Business consulting services are not just for big business. From help with licensing to financial oversight to branding, a small business consultant can help you focus on what you do best: your daily business operations. "
                 mb="44px"
               />
 
@@ -35,15 +35,15 @@ const AboutSectionOne = () => {
               >
                 <div className="mx-[-12px] flex flex-wrap">
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Premium quality" />
-                    <List text="Tailwind CSS" />
-                    <List text="Use for lifetime" />
+                    <List text="Business Registration" />
+                    <List text="Licensing" />
+                    <List text="Branding" />
                   </div>
 
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Next.js" />
-                    <List text="Rich documentation" />
-                    <List text="Developer friendly" />
+                    <List text="Web Development" />
+                    <List text="Marketing" />
+                    <List text="Financial Oversight" />
                   </div>
                 </div>
               </div>
