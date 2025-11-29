4d23a64bdb12283722b06947dd9d9b74c7b4d5c0
Luci <Luci.Canet@gmail.com>
2025-11-29 18:27:52 -0500

    term1

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -15,11 +15,7 @@ export default function TermsPage() {
 
       <div className="container py-16">
       <p>We are Dart Consulting Corp ("Company," "we," "us," "our"), a company registered in Delaware, United States at 1201 N. Orange Street, Suite 762, Wilmington, DE 19801.</p>     
-      <p>We operate the website https://dblpark.com/ (the "Site"), as well as any other related products and services that refer or link to these legal terms (the "Legal Terms") (collectively, the "Services").</p>
-      <p>We provide a parking assistant service to help you communicate with other drivers.</p>
-      <p>You can contact us by phone at 8455390289, email at info@dartcoop.com, or by mail to 1201 N. Orange Street, Suite 762, Wilmington, DE 19801, United States.</p>
-
-      
+     
       </div>
     </>
   );
