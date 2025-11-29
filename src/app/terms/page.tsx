51451aaf2b6021cd62a9a291d347e9186244a015
Luci <Luci.Canet@gmail.com>
2025-11-29 17:14:21 -0500

    blank terms

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -14,27 +14,6 @@ export default function TermsPage() {
       />
 
       <div className="container py-16">
-        <h1>TERMS OF SERVICE</h1>
-        <p>Last updated October 14, 2025</p>
-        <br></br>
-        <h2>Agreement to our Legal Terms</h2>
-        <p>We are Dart Consulting Corp ("Company," "we," "us," "our"), a company registered in Delaware, United
-          States at 1201 N. Orange Street, Suite 762, Wilmington, DE 19801</p>
-        <p>We operate the website https://dblpark.com/ (the "Site"), as well as any other related products and services that refer or link to these legal terms (the "Legal Terms") (collectively, the "Services").
-        </p>
-        <p>We provide a parking assistant service to help you communicate with other drivers.</p>
-        <p>You can contact us by phone at 8455390289, email at info@dartcoop.com, or by mail to 1201 N. Orange Street, Suite 762, Wilmington, DE 19801, United States.</p>
-        <p>These Legal Terms constitute a legally binding agreement made between you, whether personally or on behalf of an entity ("you"), and Dart Consulting Corp, concerning your access to and use of the Services. You agree that by accessing the Services, you have read, understood, and agreed to be bound by all of these Legal Terms. IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.</p>
-        <p>Supplemental terms and conditions or documents that may be posted on the Services from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Legal Terms at any time and for any reason. We will alert you about any changes by updating the "Last updated" date of these Legal Terms, and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Legal Terms to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Legal Terms by your continued use of the Services after the date such revised Legal Terms are posted.
-        </p>
-        <p>The Services are intended for users who are at least 18 years old. Persons under the age of 18 are not permitted to use or register for the Services.
-        </p>
-        <p>We recommend that you print a copy of these Legal Terms for your records.</p>
-
-
-
-
-
       
 
 
